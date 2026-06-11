bda29c4933917bbd9b2e73c44e868e04ac14a77a
meghamittal24 <meghamittal9027@gmail.com>
2026-06-11 17:38:57 +0530

    Create Q2.cpp

---
+++ Week_2/Day_2/Q2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+      vector<int> a(26,0),b(26,0);
+      for (char c: s1)a[c-'a']++;
+      int k=s1.size();
+      for (int i=0;i<s2.size();i++){
+        b[s2[i] -'a']++;
+        if(i>=k) b[s2[i-k]-'a']--;
+        if(a==b) return true;
+      }
+      return false;
+    }
+};
